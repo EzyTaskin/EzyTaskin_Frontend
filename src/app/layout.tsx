19b4d03a85fdd4099acd4fb97327bf0e8de61e6e
Trung Nguyen <57174311+trungnt2910@users.noreply.github.com>
2025-05-02 00:23:26 +0000

    fix: Set referrer policy
    Will be useful in development mode, since many backend APIs expect the
    full URL path in the `referer` header.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,20 @@
 import {Inter} from "next/font/google";
 import "./globals.css";
 import {Suspense} from "react";
+import { Metadata } from "next";
 
 const inter = Inter({
     subsets: ["latin"],
 });
 
-export const metadata = {
+export const metadata: Metadata = {
     title: "EzyTaskin",
     description: "EzyTaskin Website",
 };
+
+if (process.env.NODE_ENV === "development") {
+    metadata.referrer = "no-referrer-when-downgrade";
+}
 
 export default function RootLayout({children}) {
     return (
