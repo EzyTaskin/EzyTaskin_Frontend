9fcbd4694451dd605340a9c52327be2794d8ebf1
notabota <nnv2205owo@gmail.com>
2025-05-11 00:36:34 +1000

    Update useQueryPayment deps

--- src/app/hooks/useQueryPayment.ts
+++ src/app/hooks/useQueryPayment.ts
@@ -1,17 +1,17 @@
-import { useState, useEffect } from "react";
-import { PaymentReceiveCardType } from "../constants/type";
-import { getPaymentMethod } from "../helpers/api/payment";
-import { useQueryCards } from "./useQueryCards";
+import {useState, useEffect} from "react";
+import {PaymentReceiveCardType} from "../constants/type";
+import {getPaymentMethod} from "../helpers/api/payment";
+import {useQueryCards} from "./useQueryCards";
 
 export default function useQueryPayment(subpage: string) {
 
-  const { cards, loadCards } = useQueryCards();
+    const {cards, loadCards} = useQueryCards();
 
-  useEffect(() => {
-    if (subpage === "payment-methods") {
-      loadCards();
-    }
-  }, [subpage]);
+    useEffect(() => {
+        if (subpage === "payment-methods") {
+            loadCards();
+        }
+    }, [subpage, loadCards]);
 
-  return { cards, loadCards };
+    return {cards, loadCards};
 }
