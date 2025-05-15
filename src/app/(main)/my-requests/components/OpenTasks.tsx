b9b6586917794a57561a0b973efb22396e09059a
Trung Nguyen <57174311+trungnt2910@users.noreply.github.com>
2025-05-15 03:48:44 +0000

    fix: Typos

--- src/app/(main)/my-requests/components/OpenTasks.tsx
+++ src/app/(main)/my-requests/components/OpenTasks.tsx
@@ -46,7 +46,7 @@ export default function OpenTasks({openTasks}: {
                                     <Link href={`/my-tasks/details?taskId=${task.id}`}>
                                         <button
                                             className="bg-indigo-600 hover:bg-indigo-700 text-white font-medium px-4 py-2 rounded-full shadow">
-                                            Task details
+                                            Task Details
                                         </button>
                                     </Link>
                                 </div>
